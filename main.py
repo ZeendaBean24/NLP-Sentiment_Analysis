bc2d66eb88acf388c27554627fbad1c0f63575fb
ZeendaBean24 <liuzeen999@gmail.com>
2024-04-24 11:49:17 -0400

    Use NLTK corpus stopwords function instead of manual stop words

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 import string
 from collections import Counter 
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
 
 text = open('read.txt', encoding='utf-8').read() # encoding - standard process
@@ -20,22 +21,10 @@ cleaned_text = lower_case.translate(str.maketrans('','',string.punctuation)) # m
 # tokenized_words = cleaned_text.split()
 tokenized_words = word_tokenize(cleaned_text, "english") #.split takes a lot more time
 
-# Stop Words
-stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself",
-              "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself",
-              "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these",
-              "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do",
-              "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-              "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-              "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
-
 # Remove stop words
 final_words = []
 for word in tokenized_words:
-    if word not in stop_words:
+    if word not in stopwords('english'): # can choose the language
         final_words.append(word)
 
 # print(final_words)
