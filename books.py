927ceb1b6d416c703c517e6e167d55f69ac7fa32
ZeendaBean24 <liuzeen999@gmail.com>
2024-05-03 09:04:01 -0400

    Implement env

--- books.py
+++ books.py
@@ -1,3 +1,11 @@
-import requests
+import requests, os
+from dotenv import load_dotenv
 
-URL = "https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=yourkey"
+load_dotenv()
+
+NYTIMES_BOOKS_API = os.getenv("NYTIMES_BOOKS_API")
+
+URL = f"https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key={NYTIMES_BOOKS_API}"
+
+print(URL)
+print(NYTIMES_BOOKS_API)
