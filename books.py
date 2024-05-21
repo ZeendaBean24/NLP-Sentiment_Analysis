f635539d93784e82033f41dae5575cf083bd9c69
ZeendaBean24 <liuzeen999@gmail.com>
2024-05-21 11:15:31 -0400

    Add modules and all functions required to books.py

--- books.py
+++ books.py
@@ -1,6 +1,11 @@
-import requests, os, json, random
+import requests, os, json, random, string
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+from collections import Counter
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import matplotlib.pyplot as plt
 
 load_dotenv()
 
@@ -32,3 +37,48 @@ if reviews:
 else: # If the query has no returns
     print("No reviews found.")
 
+def process_text(text):
+    lower_case = text.lower()
+    cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
+    tokenized_words = word_tokenize(cleaned_text, "english")
+    
+    final_words = [word for word in tokenized_words if word not in stopwords.words('english')]
+    
+    return final_words
+
+def analyze_emotions(final_words):
+    emotion_list = []
+    with open('emotions.txt', 'r') as file:
+        for line in file:
+            clear_line = line.replace("n", '').replace(",", '').replace("'", '').strip()
+            word, emotion = clear_line.split(':')
+            if word in final_words:
+                emotion_list.append(emotion)
+    
+    return Counter(emotion_list)
+
+def sentiment_analyse(sentiment_text):
+    score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+    print(f"Overall score: {score}\n") 
+    neg = score['neg']
+    pos = score['pos']
+    if neg > pos:
+        print("Overall: Negative Sentiment")
+    elif pos > neg:
+        print("Overall: Positive Sentiment")
+    else:
+        print("Overall: Neutral Sentiment.")
+    print("\n------------------\n")
+
+def plot_emotions(emotion_counts):
+    fig, ax1 = plt.subplots()
+    ax1.bar(emotion_counts.keys(), emotion_counts.values(), color='skyblue')
+    ax1.set_title('Count of Sentiments in the Text', fontsize=14, fontweight='bold')
+    ax1.set_xlabel('Sentiments', fontsize=12, fontweight='bold')
+    ax1.set_ylabel('Count', fontsize=12, fontweight='bold')
+    ax1.tick_params(axis='x', rotation=45)
+    ax1.tick_params(axis='both', which='major', labelsize=10)
+    plt.grid(axis='y', linestyle='--', linewidth=0.7)
+    fig.tight_layout()
+    # plt.savefig('graph.png')
+    plt.show()
