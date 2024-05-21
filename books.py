7c7ff30e46d073a4637b07031aca2ee9cd211080
ZeendaBean24 <liuzeen999@gmail.com>
2024-05-21 11:20:50 -0400

    Implement functions (notice how the emotions.txt is not enough).

--- books.py
+++ books.py
@@ -21,22 +21,6 @@ def get_author_book_reviews(name):
     review_urls = [[result['summary'], result['book_title'], result['book_author']] for result in data['results'] if 'summary' in result and result['summary']]
     return review_urls
 
-author_name = input("Enter the author's name: ")
-reviews = get_author_book_reviews(author_name)
-# print(review_urls)
-
-# Select a random review from the list
-if reviews: 
-    random_review = random.choice(reviews)
-    print("------------------ \n")
-    print("Randomly selected review: \n")
-    print(f"Summary: {random_review[0]}")
-    print(f"Book Title: {random_review[1]}")
-    print(f"Author: {random_review[2]}")
-    print("\n------------------")
-else: # If the query has no returns
-    print("No reviews found.")
-
 def process_text(text):
     lower_case = text.lower()
     cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
@@ -82,3 +66,35 @@ def plot_emotions(emotion_counts):
     fig.tight_layout()
     # plt.savefig('graph.png')
     plt.show()
+
+# ------------ Implementation of Functions ------------
+author_name = input("Enter the author's name: ")
+reviews = get_author_book_reviews(author_name)
+# print(review_urls)
+
+# Select a random review from the list
+if reviews: 
+    random_review = random.choice(reviews)
+    print("------------------ \n")
+    print("Randomly selected review: \n")
+    print(f"Summary: {random_review[0]}")
+    print(f"Book Title: {random_review[1]}")
+    print(f"Author: {random_review[2]}")
+    print("\n------------------")
+    # Process the summary text for NLP
+    final_words = process_text(random_review[0])
+    
+    # Analyze emotions
+    emotion_counts = analyze_emotions(final_words)
+    
+    print(f"------------------ \n\nAll emotions detected: {list(emotion_counts.elements())}")
+    print(f"\nCounter: {emotion_counts}\n")
+    
+    # Perform sentiment analysis
+    sentiment_analyse(random_review[0])
+    
+    # Plot emotions
+    plot_emotions(emotion_counts)
+    
+else: # If the query has no returns
+    print("No reviews found.")
