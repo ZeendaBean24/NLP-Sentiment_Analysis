da84d7bc9e4466c09ddb13f0e8651e4f210bd2e0
ZeendaBean24 <liuzeen999@gmail.com>
2024-05-21 22:48:37 -0400

    Create basic structure using absolute positioning of "NLP!"

--- learn-nlp/src/NLPPathway.jsx
+++ learn-nlp/src/NLPPathway.jsx
@@ -4,10 +4,27 @@ import './NLPPathway.css';
 function NLPPathway() {
   return (
     <div className="nlp-container">
-      <div className="nlp-letter" onClick={() => alert("Manual NLP Techniques")}>N</div>
-      <div className="nlp-letter" onClick={() => alert("NLTK Sentiment Analysis")}>L</div>
-      <div className="nlp-letter" onClick={() => alert("Applying New York Times API")}>P</div>
-      <div className="nlp-exclamation" onClick={() => alert("Using Hugging Face for Movie Reviews")}>!</div>
+      {/* N Nodes */}
+      <div className="node main-node n1">N</div>
+      <div className="node minor-node n2"></div>
+      <div className="node minor-node n3"></div>
+      <div className="node minor-node n4"></div>
+      
+      {/* L Nodes */}
+      <div className="node main-node l1">L</div>
+      <div className="node minor-node l2"></div>
+      <div className="node minor-node l3"></div>
+      
+      {/* P Nodes */}
+      <div className="node main-node p1">P</div>
+      <div className="node minor-node p2"></div>
+      <div className="node minor-node p3"></div>
+      <div className="node minor-node p4"></div>
+      
+      {/* ! Nodes */}
+      <div className="node main-node exclamation1">!</div>
+      <div className="node minor-node exclamation2"></div>
+      <div className="node minor-node exclamation3"></div>
     </div>
   );
 }
