e3363b86c13c69f02ea18a43ae434c5c710e6fdf
ZeendaBean24 <liuzeen999@gmail.com>
2024-05-22 20:51:02 -0400

    Z-index higher than stars, make animation mostly in red and less in orange

--- learn-nlp/src/NLPPathway.jsx
+++ learn-nlp/src/NLPPathway.jsx
@@ -14,29 +14,29 @@ function NLPPathway() {
       </div>
       <div className="nlp-container">
         {/* N Nodes */}
-        <div className="node main-node n1"></div>
-        <div className="node minor-node n2"></div>
-        <div className="node minor-node n3"></div>
-        <div className="node minor-node n4"></div>
+        <div className="node main-node n1">1</div>
+        <div className="node minor-node n2">1.1</div>
+        <div className="node minor-node n3">1.2</div>
+        <div className="node minor-node n4">1.3</div>
         <div className="letter n">N</div>
         
         {/* L Nodes */}
-        <div className="node main-node l1"></div>
-        <div className="node minor-node l2"></div>
-        <div className="node minor-node l3"></div>
+        <div className="node main-node l1">2</div>
+        <div className="node minor-node l2">2.1</div>
+        <div className="node minor-node l3">2.2</div>
         <div className="letter l">L</div>
         
         {/* P Nodes */}
-        <div className="node main-node p1"></div>
-        <div className="node minor-node p2"></div>
-        <div className="node minor-node p3"></div>
-        <div className="node minor-node p4"></div>
+        <div className="node main-node p1">3</div>
+        <div className="node minor-node p2">3.1</div>
+        <div className="node minor-node p3">3.2</div>
+        <div className="node minor-node p4">3.3</div>
         <div className="letter p">P</div>
         
         {/* ! Nodes */}
-        <div className="node main-node exclamation1"></div>
-        <div className="node minor-node exclamation2"></div>
-        <div className="node end-node exclamation3"></div>
+        <div className="node main-node exclamation1">4</div>
+        <div className="node minor-node exclamation2">4.1</div>
+        <div className="node end-node exclamation3">4.2</div>
         <div className="letter ex">!</div>
 
         <svg class="fuel n" viewBox="0 0 64 64">
